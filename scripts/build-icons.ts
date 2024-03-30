df0a8d4765efebce09b83dbd34f5c3dc70929cd8
Alex <8125011+alex-kinokon@users.noreply.github.com>
2024-03-30 03:18:01 -0400

    Update new version

--- scripts/build-icons.ts
+++ scripts/build-icons.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env tsx
 import fs from "fs";
 import glob from "fast-glob";
+import JSON5 from "json5";
 
 fs.mkdirSync("./dist/icons", { recursive: true });
 
-const iconBase = require("react-icons/lib/cjs/iconBase");
+const iconBase = require("react-icons/lib");
 iconBase.GenIcon = (icon: unknown) => () => icon;
 
 const libs = glob.sync("react-icons/*/index.js", { cwd: "./node_modules" });
@@ -14,22 +15,24 @@ const indexDefs = fs.createWriteStream("./dist/all.d.ts");
 const iconListList: [string, string[]][] = [];
 
 for (const lib of libs) {
+  if (lib === "react-icons/lib/index.js") continue;
+
   const name = lib.split("/")[1];
   const iconList: string[] = [];
   // iconList.push(name);
   const icons = fs.createWriteStream(`./dist/icons/${name}.js`);
   const defs = fs.createWriteStream(`./dist/icons/${name}.d.ts`);
 
-  defs.write(`import type { IconTree } from "./index.d";\n\n`);
+  defs.write(`import type { IconTree } from "../index.d";\n\n`);
 
-  for (let [key, value] of Object.entries(require(lib))) {
+  for (let [key, value] of Object.entries(require(lib.replace("/index.js", "")))) {
     if (name === "fa6") {
       key = key.replace(/^Fa/, "Fa6");
     } else if (name === "hi2") {
       key = key.replace(/^Hi/, "Hi2");
     }
 
-    icons.write(`export const ${key} = ${JSON.stringify((value as any)())};\n`);
+    icons.write(`export const ${key} = ${JSON5.stringify((value as any)())};\n`);
     defs.write(`export const ${key}: IconTree;\n`);
     iconList.push(key);
   }
